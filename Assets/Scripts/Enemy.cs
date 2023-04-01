f1cc57da9d3760316a59171f45b279a6a584bd67
Urukagall <dany.jorge@free.fr>
2023-04-02 00:54:04 +0200

    Enemy animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -14,10 +14,10 @@ public class Enemy : MonoBehaviour
     CurrencyManager currencyManager;
 
     public event Action OnDeath;
+    public Animator animator;
 
     private void Start()
     {
-
         currencyManager = FindObjectOfType<CurrencyManager>();
     }
     public void SetPath(Path path)
@@ -37,8 +37,62 @@ public class Enemy : MonoBehaviour
         {
 
             Vector3 dir = target - transform.position;
+
+
+            if (dir != Vector3.zero)
+            {
+                dir = dir.normalized; // Normalize the direction vector
+                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                if (angle < 0)
+                {
+                    angle += 360;
+                }
+                // angle is now the angle in degrees between the transform and the target
+                if (angle >= 45 && angle < 135)
+                {
+                    if (!animator.GetBool("IsWalkingUp"))
+                    {
+                        animator.SetBool("IsWalkingUp", true);
+                        animator.SetBool("IsWalkingRight", false);
+                        animator.SetBool("IsWalkingDown", false);
+                        animator.SetBool("IsWalkingLeft", false);
+                    }
+                }
+                else if (angle >= 135 && angle < 225)
+                {
+                    if (!animator.GetBool("IsWalkingLeft"))
+                    {
+                        animator.SetBool("IsWalkingLeft", true);
+                        animator.SetBool("IsWalkingRight", false);
+                        animator.SetBool("IsWalkingDown", false);
+                        animator.SetBool("IsWalkingUp", false);
+                    }
+                }
+                else if (angle >= 225 && angle < 315)
+                {
+                    if (!animator.GetBool("IsWalkingDown"))
+                    {
+                        animator.SetBool("IsWalkingDown", true);
+                        animator.SetBool("IsWalkingRight", false);
+                        animator.SetBool("IsWalkingLeft", false);
+                        animator.SetBool("IsWalkingUp", false);
+                    }
+                }
+                else
+                {
+                    if (!animator.GetBool("IsWalkingRight"))
+                    {
+                        animator.SetBool("IsWalkingRight", true);
+                        animator.SetBool("IsWalkingDown", false);
+                        animator.SetBool("IsWalkingLeft", false);
+                        animator.SetBool("IsWalkingUp", false);
+                    }
+                }
+            }
             transform.Translate(speed * Time.deltaTime * dir.normalized, Space.World);
         }
+
+
 
         if (Vector3.Distance(transform.position, target) <= 0.2)
         {
