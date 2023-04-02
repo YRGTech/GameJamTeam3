0eaf5066e3967c68bd6d7280ddf322b39378c696
Urukagall <dany.jorge@free.fr>
2023-04-02 11:46:29 +0200

    s

---
+++ Assets/Scripts/ShopButtonScript.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class ShopButtonScript : MonoBehaviour
+{
+    public Color hoverColor;
+    public Color startColor;
+    private SpriteRenderer rend;
+    private GameObject turret;
+    [SerializeField] int price;
+
+    public GameObject turret1;
+
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+
+    [SerializeField] NodeScript nodeScript;
+
+    [SerializeField] TextMeshPro priceText;
+
+    private CurrencyManager currencyManager;
+    private int playerId;
+
+    void Start()
+    {   
+        priceText.text = price.ToString();
+        currencyManager = FindObjectOfType<CurrencyManager>();
+        rend = GetComponent<SpriteRenderer>();
+        startColor = rend.material.color;
+    }
+    private void Update()
+    {
+        Tower tower = GetComponentInChildren<Tower>();
+        if (tower != null)
+        {
+            playerId = tower.ownerId;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Impossible de construire ici, il y a déja une tourelle.");
+            return;
+        }
+
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        if (currencyManager.CheckCurrency(playerId) >= price)
+        {
+
+            GameObject newObject = Instantiate(turret1, transform.parent.position + new Vector3(0, 0.5f), transform.rotation);
+            nodeScript.turret = newObject;
+            //newObject.transform.SetParent(transform.root);
+
+            currencyManager.AddCurrency(-price, playerId);
+
+            GameManager.Destroy(button1);
+            GameManager.Destroy(button2);
+            GameManager.Destroy(button3);
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = Color.red;
+    }
+
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
+
+
+
+
+
